8bcf13d96a3954e94092d4751d5bae8957f85b27
Cydnie Na <111486754+cydniena@users.noreply.github.com>
2024-05-24 11:18:11 +0800

    Add react and spring app

---
+++ src/test/java/com/example/queueSystem/QueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.queueSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
