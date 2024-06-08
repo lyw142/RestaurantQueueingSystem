c999f3300cdfc2f3eeeab21ccbe5ee0d35869195
Cydnie Na <111486754+cydniena@users.noreply.github.com>
2024-06-08 18:50:02 +0800

    Add user class

--- src/main/java/com/example/queueSystem/QueueSystemApplication.java
+++ src/main/java/com/example/queueSystem/QueueSystemApplication.java
@@ -2,11 +2,14 @@ package com.example.queueSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 public class QueueSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QueueSystemApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/queueSystem/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.queueSystem.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long phoneNumber;
+    private String email;
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean purchaseParkingPass(){
+        return false;
+    }
+}
